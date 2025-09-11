2c29d6bf50eb1889242d44b0261d32ac91317182
Isah <isaaholiveira15@gmail.com>
2025-09-10 22:23:57 -0300

    feat: adding mvc structure

---
+++ Sistema-Hospitalar-Kafka/src/test/java/com/sistema/hospitalar/fiap/kafka/Sistema/Hospitalar/Kafka/SistemaHospitalarKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.hospitalar.fiap.kafka.Sistema.Hospitalar.Kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaHospitalarKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
